dc27791f66edbc5a225260851d20bf9e5089cb29
Sean Carolan <scarolan@hashicorp.com>
2019-05-27 14:49:35 -0500

    Update the arcade.

---
+++ files/public_html/dropdown.js
@@ -0,0 +1,14 @@
+document.write('\
+ \
+<div class="dropdown">\
+    <button class="dropbtn">Select Game</button>\
+    <div class="dropdown-content">\
+      <a href="./pacman.html">Pac-Man</a>\
+      <a href="./galaga.html">Galaga</a>\
+      <a href="./gauntlet.html">Gauntlet</a>\
+      <a href="./digdug.html">Dig Dug</a>\
+      <a href="./dragonslair.html">Dragon\'s Lair</a>\
+  </div>\
+</div>\
+\
+');
