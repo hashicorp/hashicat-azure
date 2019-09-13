c690f8a30ac387accd7f49c3bb5a12fe3edf70c7
Shelley Bess <shelley.bess@gmail.com>
2019-09-13 20:27:24 +0000

    Modify requested file

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,7 +10,7 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
+  <center><h2>Hello World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own. 
   <!-- END -->
   
