b6c0f90941ae94a15138413e3de873517e949e2c
Sean Carolan <scarolan@hashicorp.com>
2020-01-08 12:40:30 -0600

    reset for workshop.

--- main.tf
+++ main.tf
@@ -83,10 +83,6 @@ resource "azurerm_public_ip" "catapp-pip" {
 
 resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
-  tags = {
-    Billable = "true",
-    Department = "devops"
-  }
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
--- remote_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "seanc-training"
-    workspaces {
-      name = "hashicat-azure"
-    }
-  }
-}
