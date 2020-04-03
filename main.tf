cab77298101165e379dff41efd46435cc0adc5b1
Andrew Klaas <aklaas@hashicorp.com>
2020-04-03 13:03:32 +0000

    Updated my code.

--- main.tf
+++ main.tf
@@ -117,6 +117,11 @@ resource "azurerm_virtual_machine" "catapp" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+  
+  tags = {
+    Billable = "true"
+    Department = "devops"
+  }
 }
 
 # We're using a little trick here so we can run the provisioner without
