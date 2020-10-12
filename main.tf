9e806bb113ba10acba64ef77a04a745b3f9a20b1
Roger Berlind <roger@hashicorp.com>
2020-10-12 19:22:48 -0400

    add blank line before tags

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = var.location
+  
   tags = {
     environment = "Production"
   }
