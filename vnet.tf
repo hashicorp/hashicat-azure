67eadfb98326afa9f9073a528ce892869c665cff
Vignesh Prabhu <vigneshprab@gmail.com>
2020-10-14 13:55:29 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/vigneshp-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name = data.azurerm_resource_group.network.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
