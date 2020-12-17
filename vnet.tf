33facc2138224b95066d3ddfd274d870f7e11ead
Your Name <you@example.com>
2020-12-17 22:31:34 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,13 +1,6 @@
 module "vnet" {
   source  = "app.terraform.io/williwxx/vnet/azurerm"
   version = "2.3.0"
+  resource_group_name  = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
-  resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "${var.prefix}-workshop"
-  location = var.location
-
-  tags = {
-    environment = "Production"
-  }
-}
 }
