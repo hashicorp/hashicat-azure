b192f75d3c700824e8cabb0217067f6b5d9091cb
Karunakar <karunrg@gmail.com>
2021-02-11 18:35:37 +0000

    adding remote backend

---
+++ remote_backend.tf
@@ -0,0 +1,12 @@
+# If you are in a workshop...
+# Do not delete this file!
+# It's required to complete the Instruqt labs.
+terraform {
+  backend "remote" {
+    organization = "karunakar-training"
+
+    workspaces {
+      name = "hashicat-azure"
+    }
+  }
+}
