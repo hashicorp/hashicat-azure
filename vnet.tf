709a3b43e2e507b8f7af99b4b36511fd9ebca2b1
Harikishan Thakar <harikishant@gmail.com>
2021-02-27 02:18:07 +0000

    Vnet Module Test 9

--- vnet.tf
+++ vnet.tf
@@ -2,4 +2,4 @@ module "network" {
   source  = "app.terraform.io/Harikishan-training/network/azurerm"
   version = "3.3.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
-}
+}#Test9
