40c66fdbd331527af101629707173f9d1a6d5996
Harikishan Thakar <harikishant@gmail.com>
2021-02-27 00:56:54 +0000

    Network update module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Harikishan-training/network/azurerm"
   version = "3.3.0"
-  resource_group_name
   # insert required variables here
-} # update 
+}
