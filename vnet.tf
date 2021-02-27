223d8d234ccdcb65298d0d09fc89881ff4a3c7dc
Harikishan Thakar <harikishant@gmail.com>
2021-02-27 01:24:12 +0000

    Vent Module Test 6

--- vnet.tf
+++ vnet.tf
@@ -1,15 +1,14 @@
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "${var.prefix}-workshop"
-  location = var.location
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
   tags = {
-    environment = "Production"
+    environment = "dev"
+    costcenter  = "it"
   }
-}
 
-module "network" {
-  source  = "app.terraform.io/Harikishan-training/network/azurerm"
-  version = "3.3.0"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  # insert required variables here
+  depends_on = [azurerm_resource_group.myresourcegroup.name]
 }
