d6e4e575be5984e42a2638cef34a0a3d1b3aa62d
Bezesteen <derek.bullard@gmail.com>
2021-03-18 19:31:45 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "Azure/network/azurerm"
+  source  = "app.terraform.io/dbullard-highmetric-training/network/azurerm"
   version = "3.0.1"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   # insert the 1 required variable here
