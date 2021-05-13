b13eb23e4272b42a810b4ad773b5470b89906c88
cooneyc1982 <46830322+cooneyc1982@users.noreply.github.com>
2021-05-13 19:08:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -122,7 +122,8 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    department = "devops"}
 }
 
 # We're using a little trick here so we can run the provisioner without
