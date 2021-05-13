416e901a6307bf3e9fc5a505db4021faadd19ae7
root <root@p-tlgypewhyaa7-workstation.c.instruqt-prod.internal>
2021-05-13 19:17:26 +0000

    removed vnet files

--- vnet.tf
+++
@@ -1,6 +0,0 @@
-module "network" {
-  source  = "app.terraform.io/OG-training/network/azurerm"
-  version = "3.0.1"
-  # insert required variables here
-  resource_group_name = "${var.prefix}-workshop"
-}
