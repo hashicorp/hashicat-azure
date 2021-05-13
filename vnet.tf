aaed45f2e71b6bf27dc4c6fc1ca973c665c5d27f
Chuck Ott Test <chuck.ott@outlook.com>
2021-05-13 19:24:04 +0000

    test

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/ChuckOtt-Testing/network/azurerm"
-  resource_group_name = "azurerm_resource_group.appcat.guaravtest"
+  resource_group_name = "chuckott-workshop"
   version = "3.0.1"
   # insert required variables here
 }
