2d33f6b99a56b437bccb2f37838fe1ea15d510e9
Orestes Garcia <69819694+ogar2901@users.noreply.github.com>
2021-05-14 11:07:28 -0400

    Update VM size to comply with policy

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "subnet_prefix" {
 
 variable "vm_size" {
   description = "Specifies the size of the virtual machine."
-  default     = "Standard_B1s"
+  default     = "Standard_A1"
 }
 
 variable "image_publisher" {
