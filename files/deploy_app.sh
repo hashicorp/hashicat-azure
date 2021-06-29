1cd3b66db76f81eebb2938512f5f4d90b2ae6193
Eugene Lipsky <eugene.lipsky@grey.com>
2021-06-29 15:12:14 +0000

    ds

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,15 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-<<<<<<< HEAD
-<<<<<<< HEAD
-  Welcome to ${PREFIX}'s app. Can you see this? 
-=======
-  Welcome to ${PREFIX}'s app. Replace this text with your own. testing 123
->>>>>>> 34e3e533679940be940532c1d606162658f97307
-=======
   Welcome to ${PREFIX}'s app. Replace this text with your own. testing 123 4566654544
->>>>>>> eb4b91b86d5cbcb4712fc9dc6c9d5465b3c1904c
   <!-- END -->
   
   </div>
