2ebd53356e1ff341a08f15c48ac35f7f060cc001
Michael Kennedy <michaelarronkennedy@gmail.com>
2021-07-15 15:19:00 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to the abyss at the end of time. All timelines end here. ${PREFIX} is he who remains. 
   <!-- END -->
   
   </div>
