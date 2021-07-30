a737ad6a928eedb2be346f33faaacb379ed185e5
Tyler Morrison <tyler234@gmail.com>
2021-07-30 18:09:23 +0000

    changed resource group name

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Tmorrison-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = azurerm_resource_group.test.name
+  resource_group_name = azurerm_resource_group.meow
 }
