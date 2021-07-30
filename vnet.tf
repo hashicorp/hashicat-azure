685f1fe09588933100c6e0908cf4d86145b42685
Tyler Morrison <tyler234@gmail.com>
2021-07-30 18:14:54 +0000

    updated vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,14 @@
-module "network" {
+module "network" {​​​​​​​​
   source  = "app.terraform.io/Tmorrison-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = azurerm_resource_group.meow
-}
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  tags = {​​​​​​​​
+    Environment = "dev"
+    Cost-center = "it"
+  }​​​​​​​​
+}​​​​​​​​
+
+
