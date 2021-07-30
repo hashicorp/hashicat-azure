59425c16d00b30ea61f6483fd6ae53906cf5f391
Ashutosh Singh <ashtuosh.12183@gmail.com>
2021-07-30 10:12:08 +0000

    changed published app style

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,13 +4,13 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>ACME WORLD!</title></head>
   <body>
-  <div style="width:800px;margin: 0 auto">
+  <div style="width:400px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
+  <center><h2>ACME World!</h2></center>
   Welcome to ${PREFIX}'s ACME marketing app. Your own marketing app. 
   <!-- END -->
   
