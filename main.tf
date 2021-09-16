5efa51bf0afc0ef67aed6d7ba2b57ceb4386c8aa
Isadoramenezes <isadoranezes@gmail.com>
2021-09-16 20:41:23 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -136,7 +136,8 @@ resource "azurerm_virtual_machine" "catapp" {
   }
 
   tags = {
-    Department = "devops"
+    Department = "devops",
+    Billable = "true"
   }
 
   # Added to allow destroy to work correctly.
