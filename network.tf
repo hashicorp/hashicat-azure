89564be7129caba4d0064592de34a2d15fc5b3f1
Roger Berlind <roger@hashicorp.com>
2021-09-27 15:52:57 -0400

    Use network module 3.5.0

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/instruqt-circleci/network/azurerm"
-  version = "3.0.1"
+  version = "3.5.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 
   depends_on = [azurerm_resource_group.myresourcegroup]
