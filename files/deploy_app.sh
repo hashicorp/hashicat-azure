4ad62990f4af2a08c9b074bac69bd49a78e8346e
xavileci <xavileci@gmail.com>
2022-01-25 12:20:04 +0000

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,8 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to ${PREFIX}'s app.
+   1st test from GitHub 
   <!-- END -->
   
   </div>
