f95c81c5f80b276c486b506d98b2e36ccf1caf3c
Dhivya1508 <102330734+Dhivya1508@users.noreply.github.com>
2022-03-25 12:36:42 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -137,7 +137,9 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    department="devops"
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
