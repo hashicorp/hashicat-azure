b8ee1f1a2bc54ff21c0f5b53d2c9ce816c1e1b54
Stephen Ombre <stephen.ombre@gmail.com>
2022-04-19 13:14:34 +0000

    CollabVCS: add custom welcome text

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to ${PREFIX}'s app. Meow back at ya. 
   <!-- END -->
   
   </div>
