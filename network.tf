118823e18f82df466a05e7c1f8863253e3d9ca04
Vicky Elliott <velliott@medline.com>
2022-05-10 16:21:52 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -3,6 +3,5 @@ module "network" {
   source  = "app.terraform.io/Ralph-0001/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  
- resource_group_name = "rg-train-ralph"
+  resource_group_name = "rg-train-ralph"
 }
