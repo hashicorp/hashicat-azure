4ffdc27cfee35b42ca89efb70d310a146c78ff60
root <edeylin@comcast.net>
2022-05-10 14:01:06 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -6,12 +6,12 @@
 # prompted to enter it (or define it via config file or command line flags.)
 
 variable "prefix" {
-  description = "This prefix will be included in the name of most resources."
+  description = "eduard-eylin"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "Central US"
 }
 
 variable "address_space" {
@@ -73,3 +73,4 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
