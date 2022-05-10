5d40ac099ebb1897a6859a2fd0a0122e0633320c
root <edeylin@comcast.net>
2022-05-10 16:15:27 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,6 @@
+module "network" {
+  source  = "app.terraform.io/FenderEd/network/azurerm"
+  version = "3.5.0"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  # insert required variables here
+}
