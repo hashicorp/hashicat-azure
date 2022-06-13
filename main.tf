d8164058a4d5c24960917645af87db4feded4ee6
William Yang <william.yang@hashicorp.com>
2022-06-13 17:40:15 +0800

    test action

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "azurerm_public_ip" "catapp-pip" {
   allocation_method   = "Dynamic"
   domain_name_label   = "${var.prefix}-meow"
 }
-
+   
 resource "azurerm_virtual_machine" "catapp" {
   name                = "${var.prefix}-meow"
   location            = var.location
