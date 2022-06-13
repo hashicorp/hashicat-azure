a2593036aa1216858c7f4feeafbd454a8c699502
William Yang <william.yang@hashicorp.com>
2022-06-13 16:45:59 +0800

    first run

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@
 # 
 # Here is where we store the default values for all the variables used in our
 # Terraform code. If you create a variable with no default, the user will be
-# prompted to enter it (or define it via config file or command line flags.)
+# prompted  to enter it (or define it via config file or command line flags.)
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
