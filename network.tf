4186f0d7420c9a8c9a27373739be5ce0338d7a47
William Yang <william.yang@hashicorp.com>
2022-06-13 16:36:25 +0800

    remove network module first

--- network.tf
+++ network.tf
@@ -1,9 +1,9 @@
-module "network" {
-  source  = "app.terraform.io/william-trial-001/network/azurerm"
-  version = "3.5.0"
-  resource_group_name="${var.prefix}-workshop"
-  # insert required variables here
-  depends_on = [
-     azurerm_resource_group.myresourcegroup
-  ]
-}
+# module "network" {
+#   source  = "app.terraform.io/william-trial-001/network/azurerm"
+#   version = "3.5.0"
+#   resource_group_name="${var.prefix}-workshop"
+#   # insert required variables here
+#   depends_on = [
+#      azurerm_resource_group.myresourcegroup
+#   ]
+# }
