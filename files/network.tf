9db5f35b3bfd5364894c6ffb8ba86bf61f72c5eb
root <jessy.st-pierre@adsdata.ca>
2022-06-14 18:41:58 +0000

    Fixed network module source

--- files/network.tf
+++ files/network.tf
@@ -1,5 +1,6 @@
 module "network" {
-  source              = "Azure/network/azurerm"
+  source              = "app.terraform.io/adsdata/network/azurerm"
+  version             = "3.5.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
