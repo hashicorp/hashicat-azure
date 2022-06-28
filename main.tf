7f5bd4439bfd2eb5c877d853007745517d4e70f5
mluis1985 <108298144+mluis1985@users.noreply.github.com>
2022-06-28 15:21:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,6 +107,9 @@ resource "azurerm_virtual_machine" "catapp" {
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
+  tags = {
+    Department = "devops"
+  }
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
