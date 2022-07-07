1a40b302088a8df9040da318d5c2a7fa84fbc856
loudstil <loudstil@gmail.com>
2022-07-07 09:24:56 +0000

    update message

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,8 +10,9 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  <center><h2>Meow World! - Special Week </h2></center>
+  Welcome to ${PREFIX}'s app. 
+  We Have a special week !!. 
   <!-- END -->
   
   </div>
