2c75ac02920c6ab1d48ff37378ecc7b89c25ada4
Lucas Martin-King <lucas.martinking@coles.com.au>
2022-08-25 03:20:08 +0000

    Tweaks to web page

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,6 +10,7 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="https://placedog.net//${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own. 
   <!-- END -->
