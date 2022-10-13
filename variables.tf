be705fe4bd069a36128420f051b98eabc05ad986
samuellee-zy <samuelee.zy@gmail.com>
2022-10-13 15:45:25 +1100

    Updated prefix

--- variables.tf
+++ variables.tf
@@ -7,12 +7,12 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-  default     = "sam-hashicat-demo"
+  default     = "sam-hashicat"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "Australia Southeast"
 }
 
 variable "address_space" {
