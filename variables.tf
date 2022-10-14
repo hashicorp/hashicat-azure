8d3f4b1254cecb16cc5b40507aed5ba97da5532b
samuellee-zy <samuelee.zy@gmail.com>
2022-10-14 16:04:41 +1100

    Updated prefix

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-  default     = "sam-hashicat-demo"
+  default     = "sam-hashicat"
 }
 
 variable "location" {
