592caeb38bd941d55d184c641e1b9c73a323be2b
samuellee-zy <samuelee.zy@gmail.com>
2022-10-18 09:18:23 +1100

    Added some changes

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "${var.prefix}-workshop"
+  name     = "${var.prefix}"
   location = var.location
 
   tags = {
