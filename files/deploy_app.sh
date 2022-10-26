59f283b9fb4726d2b4a83591590219f050411cf0
Doopdubudubudoop <114286167+Doopdubudubudoop@users.noreply.github.com>
2022-10-26 15:56:52 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>DOOP!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  <center><h2>DoopdubudubuDoop World!</h2></center>
+  Welcome to ${PREFIX}'s app. This place is my own, so away from HERE. 
   <!-- END -->
   
   </div>
