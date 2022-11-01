89cc1076dfed06e3ce66cb63edaa2967f436ddde
greglogsdon <logsdonj@greglogsdon.com>
2022-11-01 18:26:13 +0000

    Added the firt tag

--- main.tf
+++ main.tf
@@ -107,6 +107,7 @@ resource "azurerm_virtual_machine" "catapp" {
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
+  department          = devops
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
