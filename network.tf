6fab4289cb7c588453b5fc20d83689fcd9c5b5f6
greglogsdon <logsdonj@greglogsdon.com>
2022-11-01 19:16:26 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "greglogsdon"
+  location = "centralus"
+}
 
 module "network" {
   source  = "app.terraform.io/GREGLOGSDON-training/network/azurerm"
@@ -5,7 +13,7 @@ module "network" {
   resource_group_name = azurerm_resource_group.myresourcegroup.name
  
   depends_on = [azurerm_resource_group.myresourcegroup]
-}
+    }
 
-  depends_on = [azurerm_resource_group.example]
+     depends_on = [azurerm_resource_group.example]
 }
