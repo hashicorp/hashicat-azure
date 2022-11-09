bc5395f8e245a72b7e364f3b22ce4067c7439466
ruaidhri cook <ruaidhri.cook@h-f.co.uk>
2022-11-09 16:40:05 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -135,7 +135,9 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    Department = "devops"
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
