a3ebf0d7aeac9f99b9f653e3a407117052633238
jbradsha1 <117884375+jbradsha1@users.noreply.github.com>
2022-12-02 12:14:46 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to ${PREFIX}'s app. Replace this text with your own!!! 
   <!-- END -->
   
   </div>
@@ -19,4 +19,4 @@ cat << EOM > /var/www/html/index.html
 </html>
 EOM
 
-echo "Script complete."
+echo "Script complete!!!!"
