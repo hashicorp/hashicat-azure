84d6fcfa05c1c3f817f743d2d852ea1fcf89ac6d
senpi <io.ti.sento@gmail.com>
2022-12-21 15:59:59 +0900

    Update deploy_app.sh
    変更しました。

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. 世界！最強！スローガン！
+  Welcome to ${PREFIX}'s app. 世界！最強！スローガン！！
   <!-- END -->
   
   </div>
