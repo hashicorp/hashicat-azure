c98219b897dda01d71a5388afd55b15df6f5a7de
Navdeep <nkaur@payments.ca>
2023-02-16 20:31:35 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/Payments_2023/network/azurerm"
   version = "3.5.0"
-   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
