008332dbf4cefab2b56ba13f29828b95301b47a4
Navdeep <nkaur@payments.ca>
2023-02-16 20:27:18 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,31 +1,6 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "Canada Central"
-}
-
 module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  subnet_service_endpoints = {
-    "subnet1" : ["Microsoft.Sql"],
-    "subnet2" : ["Microsoft.Sql"],
-    "subnet3" : ["Microsoft.Sql"]
-  }
-
-  version= "3.5.0"
-  use_for_each = true
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-  }
-
-  depends_on = [azurerm_resource_group.example]
+  source  = "app.terraform.io/Payments_2023/network/azurerm"
+  version = "3.5.0"
+   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  # insert required variables here
 }
