87384bb6e425e7806e5d09f88d2cc9d26317181a
Navdeep <nkaur@payments.ca>
2023-02-16 19:47:28 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -130,7 +130,10 @@ resource "azurerm_linux_virtual_machine" "catapp" {
 
   }
 
-  tags = {}
+  tags = {
+    Department - "DevOps"
+    Billalbe = "true"
+  }
 
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
