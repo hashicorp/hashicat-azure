f047e1cf5641aa4036dd2ebb10afe442224bd01e
Navdeep <nkaur@payments.ca>
2023-02-16 20:35:35 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "azurerm_linux_virtual_machine" "catapp" {
 
   tags = {
     Department = "DevOps"
-    Billalbe = "true"
+    Billable = "true"
   }
 
   # Added to allow destroy to work correctly.
