4d3870ef5615175749707102b2e738152d77bd39
seniuka <aseniuk@gmail.com>
2023-03-01 21:16:56 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/adamseniuk-training/network/azurerm"
   version             = "3.5.0"
-  resource_group_name = azurerm_resource_group.gaurav
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
