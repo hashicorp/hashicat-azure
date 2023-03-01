3014fbb3baace56df692360d253d95e291900b98
seniuka <aseniuk@gmail.com>
2023-03-01 20:57:11 +0000

    Added network module... again

--- network.tf
+++ network.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "gaurav" {
 module "network" {
   source              = "Azure/network/azurerm"
   version             = "3.5.0"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.gaurav
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
