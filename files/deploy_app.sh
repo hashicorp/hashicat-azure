d4a05d1aec4661e685cf570d9a01639489f916fd
Siraj-Lakhani <113131157+Siraj-Lakhani@users.noreply.github.com>
2023-03-01 13:53:59 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. This is my personal Page. 
+  Welcome to ${PREFIX}'s app. This is my personal Personal Page. 
   <!-- END -->
   
   </div>
