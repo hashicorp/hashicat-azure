652ef8a67d995e343a261105528c98a0f98f4527
Mark Nelson <36041381+f40felix@users.noreply.github.com>
2023-03-29 13:38:47 +0100

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Its f40felixs funky site. 
+  Welcome to ${PREFIX}'s app. Its f40felixs new funky site. 
   <!-- END -->
   
   </div>
