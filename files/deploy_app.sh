df20df6648377cc20ecf35ba7df71665ee737366
Salvador Facio <el_fackoo@hotail.com>
2023-03-29 04:10:47 +0000

    test

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,8 +10,8 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  <center><h2>Meow World Perro!</h2></center>
+  Welcome to ${PREFIX}'s app. Perro perrín perrón. 
   <!-- END -->
   
   </div>
