7e9cd82c3f9f52c8b1961c641b2151410bad5990
cscholer123 <callie.scholer@gmail.com>
2023-04-06 19:02:13 +0000

    fixing shit

--- main.tf
+++ main.tf
@@ -115,8 +115,7 @@ resource "azurerm_linux_virtual_machine" "catapp" {
   admin_password                  = var.admin_password
   disable_password_authentication = false
   network_interface_ids           = [azurerm_network_interface.catapp-nic.id]
-  Department                      = devops
-  Billable                        = true
+ 
 
   source_image_reference {
     publisher = var.image_publisher
