7dd810135f867d99036b7a2d3313e7e6b6123aa1
banjman81 <benjamin.man97@gmail.com>
2023-05-02 19:20:27 +0000

    Added first tag

--- main.tf
+++ main.tf
@@ -118,6 +118,7 @@ resource "azurerm_linux_virtual_machine" "catapp" {
   admin_password                  = var.admin_password
   disable_password_authentication = false
   network_interface_ids           = [azurerm_network_interface.catapp-nic.id]
+  Department                      = "devops"
 
   source_image_reference {
     publisher = var.image_publisher
