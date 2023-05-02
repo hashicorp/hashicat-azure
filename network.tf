f3ebf2aa9ab914718621914fa437d05a8a199d77
tjgitdh <109803925+tjgitdh@users.noreply.github.com>
2023-05-02 19:43:21 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Tj-test-tcloud/network/azurerm"
   version = "3.5.0"
-  resource_group_name = 
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
