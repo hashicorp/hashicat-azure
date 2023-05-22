99e4824e4c67c15d7397af09599e0306790934f5
thom002 <thomas.ribet@devoteam.com>
2023-05-22 14:15:14 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,20 @@
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.myresourcegroup
+  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  subnet_service_endpoints = {
+    "subnet1" : ["Microsoft.Sql"],
+    "subnet2" : ["Microsoft.Sql"],
+    "subnet3" : ["Microsoft.Sql"]
+  }
+  use_for_each = true
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.myresourcegroup]
+}
