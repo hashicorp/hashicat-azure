3764c731aa77e64b0064514fccc808bd08113d5e
John Muller <john.muller@8451.com>
2023-11-30 18:44:10 +0000

    change the slogan

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -14,7 +14,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to ${PREFIX}'s app. It's very much an app.
   <!-- END -->
   
   </div>
